57c630dde6e05844e0d1f78718efface60196dd8
KAldhalimi <karrarmajed@gmail.com>
2019-02-12 22:21:52 -0500

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using ASPPractice1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASPPractice1.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult AddUser(Registration u)
+        {
+            ViewBag.FirstName = u.FirstName;
+            return View();
+        }
+    }
+}
